c98517243d7e29198d638b270083881af35dec15
Thirumoorthi R <thirumoorthi.r@outlook.com>
2015-07-17 13:40:11 +0530

    Create Mount_remote_directory.sh

---
+++ Mount_remote_directory.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This script is written by Thirumoorthi, @thiru978 for his personal use in his system
+
+# first lets check did mount is already happened 
+
+l=$(mount | grep 167| wc|awk 'FS="\t" {print $1}')
+
+echo $l
+
+d=$(ls -d  | grep Ads| wc |awk 'FS="\t" {print $1}')
+
+echo $d
+
+if [ $d == 0 ];then
+mkdir Ads
+echo "Ads Directory created for the first time use"
+fi
+echo "***Ads mount point Already available no worriers****"
+
+
+if  [ $l -gt 0 ] ;
+then
+echo " AMEX Directory is already mounted***YEEEEHHHHHHH****"
+fi
+ 
+
+n=tramasw
+cd ~$USERNAME/Desktop/Ads
+
+echo "Please Enter your AMEX ADS"
+# mount.cifs //"Remote Server IP Address"/TSHome$/$n /home/$USERNAME/Desktop/Ads -o user=$n,domain=ADS
