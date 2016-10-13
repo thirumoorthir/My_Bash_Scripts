53ae2801988591ae2b7b54666444038c617198ec
Thirumoorthi R <thirumoorthi.r@outlook.com>
2016-10-13 16:05:04 +0530

    Update Generateplugin.sh

--- Generateplugin.sh
+++ Generateplugin.sh
@@ -1,26 +1,23 @@
 #this is the file for runnig generate plugin 
 #! /usr/bin/ksh
-
-my_array=( `ls` )
+echo "Please enter NDM name , which you want to run Generate Plugin script"
+read ndm
+my_array=( `ls -d */ /basefolder/$ndm ` )
 my_array_length=${#my_array[@]}
 b=$my_array_length
 a=0
-echo $my_array_length
-
-
+#echo $my_array_length
 for element in "${my_array[@]}"
 do
-   cd /amex/jg02/"$element"deployment
+   cd /basefolder/$ndm/"$element"deployment
    pwd
    ./generate_plugin.ksh
-   
    for i in ${!my_array[@]} ; do  
     if [[ ${my_array[$i]} = $element ]]; then 
       my_array[$i]=''
       ((a+=1)) 
-      print " $a of $b folders finished , "  
-      echo  " $a of $b folders finished , "  
+      print " $a of $b folders finished , " # this command is for Korn Shell
+      echo  " $a of $b folders finished , " # This is for Bourne again shell
     fi
-    
 done
 done
